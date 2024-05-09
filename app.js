5ed08981be81689121706b0ec0a0f05a6f7ff95b
risan <potallegta@gmail.com>
2024-05-09 12:35:57 +0800

    feat: style slider & reset

---
+++ app.js
@@ -0,0 +1,36 @@
+Vue.createApp({
+  data() {
+    return {
+      box: {
+        perspective: 100,
+        rotateX: 0,
+        rotateY: 0,
+        rotateZ: 0,
+      },
+    };
+  },
+  computed: {
+    box_styles() {
+      return {
+        transform: `
+        perspective(${this.box.perspective}px) 
+        rotateX(${this.box.rotateX}deg) 
+        rotateY(${this.box.rotateY}deg) 
+        rotateZ(${this.box.rotateZ}deg)
+        `,
+      };
+    },
+  },
+  methods: {
+    reset() {
+      for (const key in this.box) {
+        let newVal = 0;
+
+        if (key === "perspective") {
+          newVal = 100;
+        }
+        this.box[key] = newVal;
+      }
+    },
+  },
+}).mount("#app");
